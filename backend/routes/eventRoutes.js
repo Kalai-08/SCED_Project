ee1a7352505bb338828650337dab5acd50acf993
Kalai <kalaiblazer14@gmail.com>
2026-04-17 22:13:26 +0530

    Revert "feat: API Connection Bridge and Project Restructure"

--- backend/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Ensure this points to your specific controller file
-const { getEvents } = require('../controllers/todoController'); 
-
-router.get('/', getEvents);
-
-module.exports = router;
