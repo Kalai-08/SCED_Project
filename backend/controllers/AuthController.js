015dc8b18416395eefb43b1cfdbf7bc8bf060f1c
Kalai <kalaiblazer14@gmail.com>
2026-07-02 13:53:14 +0530

    Fixed url

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -109,7 +109,7 @@ const forgotPassword = async (req, res) => {
                 [token, expires, user.id],
             );
 
-            const resetLink = `${process.env.FRONTEND_URL || 'http://localhost:5173'}/reset-password/${token}`;
+            const resetLink = `${process.env.FRONTEND_URL || 'http://localhost:5173'}/#/reset-password/${token}`;
             await sendPasswordReset(user, resetLink);
         }
 
